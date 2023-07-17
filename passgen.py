73842f4d76db41ec75b1664e2dd1039a5e4f878e
Oleksii Popov <popov.scar@gmail.com>
2023-07-17 15:07:20 +0300

    New algorithm

--- passgen.py
+++ passgen.py
@@ -1,5 +1,7 @@
 import random
 import sys
+
+import math
 import pyperclip
 
 
@@ -11,12 +13,12 @@ class RandomPassword:
 
     def __init__(self):
         print(
-            "______             _____\n" +
-            "| ___ \\           |  __ \\\n" +
-            "| |_/ /_ _ ___ ___| |  \\/ ___ _ __\n" +
-            "|  __/ _` / __/ __| | __ / _ \\ '_ \\\n" +
-            "| | | (_| \\__ \\__ \\ |_\\ \\  __/ | | |\n" +
-            "\\_|  \\__,_|___/___/\\____/\\___|_| |_|"
+            "\t______             _____\n" +
+            "\t| ___ \\           |  __ \\\n" +
+            "\t| |_/ /_ _ ___ ___| |  \\/ ___ _ __\n" +
+            "\t|  __/ _` / __/ __| | __ / _ \\ '_ \\\n" +
+            "\t| | | (_| \\__ \\__ \\ |_\\ \\  __/ | | |\n" +
+            "\t\\_|  \\__,_|___/___/\\____/\\___|_| |_|"
         )
 
         self.__run(
@@ -25,19 +27,14 @@ class RandomPassword:
         )
 
     def __run(self, without_asks: bool = False, to_clipboard: bool = False):
-        if without_asks:
-            length = self.PASSWORD_LENGTH
-            complexity = self.ALPHABETS + self.ALPHABETS.upper() + self.NUMBERS + self.SPECIALS
-        else:
-            length = self.__ask_password_length()
-            complexity = self.__ask_complexity_password()
-
+        length = self.PASSWORD_LENGTH if without_asks else self.__ask_password_length()
+        complexity = self.__ask_complexity_password(length, without_asks)
         generated_pass = "".join(random.sample(complexity, length))
 
-        print('=' * 40)
+        self.print_line()
         print("Password Length: %s" % length)
         print("Generated Password: \033[1;33m%s\033[0m" % generated_pass)
-        print('=' * 40)
+        self.print_line()
 
         self.__ask_copy_to_clipboard(
             generated_password=generated_pass,
@@ -45,7 +42,7 @@ class RandomPassword:
         )
 
     def __ask_password_length(self) -> int:
-        print('=' * 40)
+        self.print_line()
         input_length = input(
             "\t\033[0;36mHow many symbols must have your password? " +
             "(Default length \033[1;36m%d\033[0;36m symbols):\033[0m "
@@ -59,17 +56,36 @@ class RandomPassword:
 
         return self.PASSWORD_LENGTH if input_length <= 0 else input_length
 
-    def __ask_complexity_password(self) -> str:
-        complexity = self.ALPHABETS
+    def __ask_complexity_password(self, length: int, without_asks: bool = False) -> str:
+        sections_count: int = 4
+        choice: dict = {
+            'uppercase': True,
+            'numbers': True,
+            'specials': True
+        }
+        asks: dict = {
+            'uppercase': '\t\033[0;36mUse uppercase letter? [\033[1;36mY\033[0;36m/n]:\033[0m ',
+            'numbers': '\t\033[0;36mUse numbers? [\033[1;36mY\033[0;36m/n]:\033[0m ',
+            'specials': '\t\033[0;36mUse special chars? [\033[1;36mY\033[0;36m/n]:\033[0m '
+        }
 
-        if (input('\t\033[0;36mUse uppercase letter? [\033[1;36mY\033[0;36m/n]:\033[0m ') or 'Y') in ['Y', 'y']:
-            complexity += self.ALPHABETS.upper()
+        if not without_asks:
+            for complexity_type, question in asks.items():
+                if (input(question) or 'Y') not in ['Y', 'y']:
+                    sections_count -= 1
+                    choice[complexity_type] = False
 
-        if (input('\t\033[0;36mUse numbers? [\033[1;36mY\033[0;36m/n]:\033[0m ') or 'Y') in ['Y', 'y']:
-            complexity += self.NUMBERS
+        length_section: int = math.ceil(length / sections_count)
+        complexity = "".join(random.sample(self.ALPHABETS, length_section))
 
-        if (input('\t\033[0;36mUse special chars? [\033[1;36mY\033[0;36m/n]:\033[0m ') or 'Y') in ['Y', 'y']:
-            complexity += self.SPECIALS
+        if choice['uppercase']:
+            complexity += "".join(random.sample(self.ALPHABETS.upper(), length_section))
+
+        if choice['numbers']:
+            complexity += "".join(random.sample(self.NUMBERS, length_section))
+
+        if choice['specials']:
+            complexity += "".join(random.sample(self.SPECIALS, length_section))
 
         return complexity
 
@@ -79,6 +95,10 @@ class RandomPassword:
 
         if ask or (input(question) or 'N') in ['Y', 'y']:
             pyperclip.copy(generated_password)
+
+    @staticmethod
+    def print_line(line_length: int = 60):
+        print('=' * line_length)
 
 
 try:
